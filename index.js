b19a63a01fc0d56c1f035b985568ca980ef52e3f
Rana-usman-ul-haq <98952692+Rana-usman-ul-haq@users.noreply.github.com>
2023-10-14 19:08:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+animateValue(obj, 0, 4712158, 12000);
+
+function animateValue(object, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    object.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const object = document.getElementById("value1");
+animateValue(object, 0, 610, 13000);
+
+function animateValue(object1, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    object1.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const object1 = document.getElementById("value2");
+animateValue(object1, 0, 311349, 14000);
+
+  function sliceSize(dataNum, dataTotal) {
+    return (dataNum / dataTotal) * 360;
+  }
+  function addSlice(sliceSize, pieElement, offset, sliceID, color) {
+    $(pieElement).append("<div class='slice "+sliceID+"'><span></span></div>");
+    var offset = offset - 1;
+    var sizeRotation = -179 + sliceSize;
+    $("."+sliceID).css({
+      "transform": "rotate("+offset+"deg) translate3d(0,0,0)"
+    });
+    $("."+sliceID+" span").css({
+      "transform"       : "rotate("+sizeRotation+"deg) translate3d(0,0,0)",
+      "background-color": color
+    });
+  }
+  function iterateSlices(sliceSize, pieElement, offset, dataCount, sliceCount, color) {
+    var sliceID = "s"+dataCount+"-"+sliceCount;
+    var maxSize = 179;
+    if(sliceSize<=maxSize) {
+      addSlice(sliceSize, pieElement, offset, sliceID, color);
+    } else {
+      addSlice(maxSize, pieElement, offset, sliceID, color);
+      iterateSlices(sliceSize-maxSize, pieElement, offset+maxSize, dataCount, sliceCount+1, color);
+    }
+  }
+  function createPie(dataElement, pieElement) {
+    var listData = [];
+    $(dataElement+" span").each(function() {
+      listData.push(Number($(this).html()));
+    });
+    var listTotal = 0;
+    for(var i=0; i<listData.length; i++) {
+      listTotal += listData[i];
+    }
+    var offset = 0;
+    var color = [
+      "cornflowerblue", 
+      "olivedrab", 
+      "orange", 
+      "tomato", 
+      "crimson", 
+      "purple", 
+      "turquoise", 
+      "forestgreen", 
+      "navy", 
+      "gray"
+    ];
+    for(var i=0; i<listData.length; i++) {
+      var size = sliceSize(listData[i], listTotal);
+      iterateSlices(size, pieElement, offset, i, 0, color[i]);
+      $(dataElement+" li:nth-child("+(i+1)+")").css("border-color", color[i]);
+      offset += size;
+    }
+  }
+  createPie(".pieID.legend", ".pieID.pie");
+  
+
+  let section_counter = document.querySelector('#section_counter');
+let counters = document.querySelectorAll('.counter-item .counter');
+
+// Scroll Animation
+
+let CounterObserver = new IntersectionObserver(
+  (entries, observer) => {
+    let [entry] = entries;
+    if (!entry.isIntersecting) return;
+
+    let speed = 200;
+    counters.forEach((counter, index) => {
+      function UpdateCounter() {
+        const targetNumber = +counter.dataset.target;
+        const initialNumber = +counter.innerText;
+        const incPerCount = targetNumber / speed;
+        if (initialNumber < targetNumber) {
+          counter.innerText = Math.ceil(initialNumber + incPerCount);
+          setTimeout(UpdateCounter, 4);
+        }
+        else {
+          counter.innerText = targetNumber;
+        }
+      }
+      UpdateCounter();
+
+      if (counter.parentElement.style.animation) {
+        counter.parentElement.style.animation = '';
+      } else {
+        counter.parentElement.style.animation = `slide-up 0.3s ease forwards ${
+          index / counters.length + 0.5
+        }s`;
+      }
+    });
+    observer.unobserve(section_counter);
+  },
+  {
+    root: null,
+    threshold: window.innerWidth > 768 ? 0.4 : 0.3,
+  }
+);
+
+CounterObserver.observe(section_counter);
---
+++ jquery/Jquery.js
@@ -0,0 +1,88 @@
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_content_title").waypoint(function(){
+        $(".table_of_content_title").addClass(
+            "animate__animated animate__backInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_one_time").waypoint(function(){
+        $(".table_of_one_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_left_time").waypoint(function(){
+        $(".table_of_left_time").addClass(
+            "animate__animated animate__backInLeft"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_right_time").waypoint(function(){
+        $(".table_of_right_time").addClass(
+            "animate__animated animate__backInRight"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_two_time").waypoint(function(){
+        $(".table_of_two_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_three_time").waypoint(function(){
+        $(".table_of_three_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_four_time").waypoint(function(){
+        $(".table_of_four_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_five_time").waypoint(function(){
+        $(".table_of_five_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_six_time").waypoint(function(){
+        $(".table_of_six_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_in_time").waypoint(function(){
+        $(".table_of_in_time").addClass(
+            "animate__animated animate__zoomInUp"
+        );
+    }, offset );
+});
+$(document).ready(function (){
+    let offset = {offset: "100%"}
+    $(".table_of_inup_time").waypoint(function(){
+        $(".table_of_inup_time").addClass(
+            "animate__animated animate__slideInUp"
+        );
+    }, offset );
+});
